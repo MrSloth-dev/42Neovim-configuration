84337a859bf4bc42ef664e89e26504ede9de201a
Joao Pedro Oliveira Moreira Albuquerque Barbosa <joao-pol@c1r11s8.42porto.com>
2024-04-15 11:42:09 +0100

    Fixed some bugs

--- nvim42.sh
+++ nvim42.sh
@@ -6,15 +6,13 @@
 #NOTE: Check if folder exists!
 
 
-
-
 git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/kickstarter}"
 mv ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/kickstarter} ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim}
 
 #Geting nvim to execute and autocomplete  NOTE :Make sure you have .zshrc, I recommend you install oh-my-zsh. https://ohmyz.sh/#install
 echo "Adding alias and autocomplete for nvim"
-"
- alias nvim="flatpak run io.neovim.nvim"
+echo "
+ alias nvim=\"flatpak run io.neovim.nvim\"
 compdef nvim="vim"
 setopt complete_aliases
 " >> ~/.zshrc
@@ -24,12 +22,13 @@ echo "Making init.lua configurations and setting up Header"
 
 
 #NOTE: HEADER for 42 NOTE: Change your username and your email AFTER the script!
-awk 'NR==280 { print "--42Header - Dont forget to change username and email!\n {\n \"Diogo-ss/42-header.nvim\",\n cmd = { \"Stdheader\" },\n keys = {\"<F1>\"},\n opts = {\n default_map = true, -- Default mapping <F1> in normal mode.\n auto_update = true, -- Update header when saving.\n user = \"username\", -- Your user.\n mail = \"your@email.com\", -- Your mail.\n -- add other options.\n },\n config = function(_, opts)\n require(\"42header\").setup(opts)\n end,\n },\n"} 1' ~/.config/nvim/init.lua > ~/temp.lua && mv temp.lua ~/.config/nvim/init.lua
+awk 'NR==275 { print "--42Header - Dont forget to change username and email!\n {\n \"Diogo-ss/42-header.nvim\",\n cmd = { \"Stdheader\" },\n keys = {\"<F1>\"},\n opts = {\n default_map = true, -- Default mapping <F1> in normal mode.\n auto_update = true, -- Update header when saving.\n user = \"username\", -- Your user.\n mail = \"your@email.com\", -- Your mail.\n -- add other options.\n },\n config = function(_, opts)\n require(\"42header\").setup(opts)\n end,\n },\n"} 1' ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/init.lua} > ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/temp.lua} && mv ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/temp.lua} ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/init.lua}
 
 
 #Settings for norminette compliance tabstop
- awk 'NR==150 { print "\n-- Tab Settings for Norminette compliance\n\nvim.opt.tabstop = 4 \nvim.opt.shiftwidth = 4\nvim.opt.expandtab = false\n"} 1' ~/.config/nvim/init.lua > ~/temp.lua
+awk 'NR==150 { print "\n-- Tab Settings for Norminette compliance\n\nvim.opt.tabstop = 4 \nvim.opt.shiftwidth = 4\nvim.opt.expandtab = false\n"} 1' ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/init.lua} > ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/temp.lua} && mv ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/temp.lua} ${XDG_CONFIG_HOME:-$HOME/.var/app/io.neovim.nvim/config/nvim/init.lua}
 
 echo "Header and tabstop are setup, Press <F1> in Normal Mode to Insert Header"
 
 echo "Make sure you edit your Username and e-mail in init.lua (Press <Space>sn - and then search init.lua)"
+echo "Please reset your terminal"
